ef24f4f8b614f91f0816498d06424945cf2d5ea2
Kravchuk Roman <kravchuk.roman02@gmail.com>
2023-10-06 19:15:36 +0800

    implement validator package

---
+++ internal/validator/validator.go
@@ -0,0 +1,65 @@
+package validator
+
+import (
+	"regexp"
+	"sync"
+)
+
+var (
+	EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+)
+
+type Validator struct {
+	Errors map[string]string
+	mu     sync.RWMutex
+}
+
+func New() *Validator {
+	return &Validator{Errors: make(map[string]string)}
+}
+
+// Valid returns true if the errors map doesn't contain any entries.
+func (v *Validator) Valid() bool {
+	return len(v.Errors) == 0
+}
+
+// AddError adds an error messageg to the map (so long as not entry already exists for
+// the given key).
+func (v *Validator) AddError(key, msg string) {
+	v.mu.Lock()
+	if _, exists := v.Errors[key]; !exists {
+		v.Errors[key] = msg
+	}
+	v.mu.Unlock()
+}
+
+// Check adds an error message to the map only if a validation check is not 'ok'.
+func (v *Validator) Check(ok bool, key, msg string) {
+	if !ok {
+		v.AddError(key, msg)
+	}
+}
+
+// In returns true if a specific value is in a list of strings.
+func In(value string, list ...string) bool {
+	for i := range list {
+		if value == list[i] {
+			return true
+		}
+	}
+	return false
+}
+
+// Matches retruns true if a string value matches a specific regex pattern.
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
+}
+
+// Unique returns true if all string values in a slice are unique.
+func Unique(values []string) bool {
+	uv := make(map[string]struct{})
+	for _, value := range values {
+		uv[value] = struct{}{}
+	}
+	return len(uv) == len(values)
+}
