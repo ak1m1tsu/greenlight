402da917dae9efa07a87902c5518a717b0004532
Kravchuk Roman <kravchuk.roman02@gmail.com>
2023-10-09 14:27:11 +0800

    implement sending activation token to user

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -57,8 +57,19 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	token, err := app.models.Tokens.New(user.ID, 3*25*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	app.background(func() {
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		data := map[string]any{
+			"userID":          user.ID,
+			"activationToken": token.Plaintext,
+		}
+
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", data)
 		if err != nil {
 			app.logger.Error(err, nil)
 		}
